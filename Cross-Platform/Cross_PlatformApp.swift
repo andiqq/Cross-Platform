25f8885f6ea204149bea421ef36f91d10bb6a018
Dr. Andreas Plagens <andreas@plagens.eu>
2022-04-13 09:58:06 +0200

    Initial Commit

---
+++ Cross-Platform/Cross_PlatformApp.swift
@@ -0,0 +1,17 @@
+//
+//  Cross_PlatformApp.swift
+//  Cross-Platform
+//
+//  Created by Dr. Andreas Plagens on 13.04.22.
+//
+
+import SwiftUI
+
+@main
+struct Cross_PlatformApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
